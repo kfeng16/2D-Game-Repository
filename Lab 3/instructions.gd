a336cf8e0734831d5ad5a61a45a0a059592fecff
Kristine Feng <kfeng16@fordham.edu>
2024-03-12 09:05:43 -0400

    Instructions disappear
    I added a script to the instructions to make them disappear once you start the game

---
+++ Lab 3/instructions.gd
@@ -0,0 +1,15 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+	if Input.is_action_pressed ("ui_right"): 
+		position.x += -500
+		
+	
