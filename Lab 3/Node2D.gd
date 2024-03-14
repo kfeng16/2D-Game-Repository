50f4a8e6c4b8362733dfd6aba886b68d76cb87ee
Johannes Bekele <76441090+JohannesSBA@users.noreply.github.com>
2024-03-14 10:37:56 -0400

    feat: Sticking applied to all popcorn pieces

--- Lab 3/Node2D.gd
+++ Lab 3/Node2D.gd
@@ -20,21 +20,21 @@ func spawn_popcorn():
 	var popcorn_instance = popcorn_scene.instantiate()
 	add_child(popcorn_instance)
 
-func _on_area_2d_area_entered(area):
-	if area.is_in_group("Bowl"):
-		print("Colliding with bowl, attempting to stick.")
-		stick_to_bowl(area.get_parent())  # Assuming the bowl's Area2D is a direct child of the bowl sprite
-	else:
-		stick_to_bowl(area.get_parent())
-		print("Collided with something else: ", area.name)
+#func _on_area_2d_area_entered(area):
+	#if area.is_in_group("Bowl"):
+		#print("Colliding with bowl, attempting to stick.")
+		#stick_to_bowl(area.get_parent())  # Assuming the bowl's Area2D is a direct child of the bowl sprite
+	#else:
+		#stick_to_bowl(area.get_parent())
+		#print("Collided with something else: ", area.name)
 	
-func stick_to_bowl(bowl):
-	global_position = Vector2(bowl.global_position.x, bowl.global_position.y)
-	stuck_to_bowl = true
-	set_process(false)
-
-
-func get_popcorn_info():
-	return {
-	"value": 10,  # As an example, each popcorn could represent 10 points.
-	}
+#func stick_to_bowl(bowl):
+	#global_position = Vector2(bowl.global_position.x, bowl.global_position.y)
+	#stuck_to_bowl = true
+	#set_process(false)
+#
+#
+#func get_popcorn_info():
+	#return {
+	#"value": 10,  # As an example, each popcorn could represent 10 points.
+	#}
--- Lab 3/Sprite2D.gd
+++ Lab 3/Sprite2D.gd
@@ -15,3 +15,23 @@ func _process(delta):
 	if position.y > get_viewport_rect().size.y:
 		position.x = randf_range(0, get_viewport_rect().size.x)
 		position.y = -texture.get_height()
+
+func _on_area_2d_area_entered(area):
+	if area.is_in_group("Bowl"):
+		print("Colliding with bowl, attempting to stick.")
+		stick_to_bowl(area.get_parent())  # Assuming the bowl's Area2D is a direct child of the bowl sprite
+	else:
+		stick_to_bowl(area.get_parent())
+		print("Collided with something else: ", area.name)
+	
+func stick_to_bowl(bowl):
+	global_position = Vector2(bowl.global_position.x, bowl.global_position.y)
+	speed = 0
+	set_process(false)
+
+
+func get_popcorn_info():
+	return {
+	"value": 10,  # As an example, each popcorn could represent 10 points.
+	}
+
