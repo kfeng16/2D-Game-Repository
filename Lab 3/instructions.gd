57f450f06b951eadbba2d8a5b1f88b526c4e3e7e
Johannes Bekele <76441090+JohannesSBA@users.noreply.github.com>
2024-03-31 14:18:16 -0400

    Update instructions
    Changed from a distance to just visibility as well as adding both left and right input, instead of just left

--- Lab 3/instructions.gd
+++ Lab 3/instructions.gd
@@ -9,7 +9,7 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
-	if Input.is_action_pressed ("ui_right"): 
-		position.x += -500
+	if Input.is_action_pressed ("ui_right") || Input.is_action_pressed ("ui_left"): 
+		visible = false
 		
 	
